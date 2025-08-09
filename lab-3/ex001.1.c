2e18f52ab53b91914234951134f203fde95bcb98
Wasumdee <68230070@kmitl.ac.th>
2025-08-09 02:40:52 +0000

    ahhhhhh

---
+++ lab-3/ex001.1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    float num1,num2;
+    printf("Enter number 1:");
+    scanf("%f", &num1);
+    printf("Enter number 2:");
+    scanf("%f", &num2);
+    if(num2 !=0)
+          printf("Calculator Menu:");
+          printf("1. +");
+          printf("2. -");
+          printf("3. *");
+          printf("4. /");
+          printf("5.  ");
+          printf("Ans:%.2f + %.2f =%.2f", num1, num2, num1+num2);
+    if
+          (printf("Error divided by zero\n"));
+    return 0;
+
+}
