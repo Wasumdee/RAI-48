40fc7e8709adb6b1a3ac20b54cdeb628894c0e03
Wasumdee <68230070@kmitl.ac.th>
2025-08-10 12:26:03 +0000

    nenene

--- lab-3/ex001.1.c
+++ lab-3/ex001.1.c
@@ -6,22 +6,28 @@ int main()
     scanf("%f", &num1);
     printf("Enter number 2:");
     scanf("%f", &num2);
-    if(num2 !=0) {
+   char d;
        printf("Calculator Menu:\n");
           printf("1. +\n");
           printf("2. -\n");
           printf("3. *\n");
-          printf("4. /\n");
-          scanf("%e", &num1);
-    } if (num1+num2) {
-          printf("Choose menu:1\n");
-          scanf("%e", &num1);         
-    } if (num1+num2){
-          printf("Ans:3+6=9");
+          printf("4. /\n"); 
+          printf("5. %% \n ");
+          printf("Choose Menu:");
+          scanf("%hhd", &d);
+    if ( d == 1){
+        printf("Ans: %.1f+%.1f=%.1f", num1, num2, num1+num2);
+    } else if ( d == 2){
+        printf("Ans: %.1f-%.1f=%.1f", num1, num2, num1-num2);
+    } else if ( d == 3){
+        printf("Ans: %.1f*%.1f=%.1f", num1, num2, num1*num2);
+    } else if ( d == 4){
+        printf("Ans: %.1f/%.1f=%.1f", num1, num2, num1/num2);
+    }  else{
+        printf("Error divided by zero\n");
     }
     return 0;
-      
-    }
+ }
          
        
     
